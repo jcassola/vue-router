80b2c0fa8991686b10897e170d370ab5f1e54ef5
Elodin <jcassola96@gmail.com>
2022-06-10 23:33:58 -0400

    make navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
+<navbar />
   <img alt="Vue logo" src="./assets/logo.png">
   <router-view />
 
 </template>
 
 <script>
-
+import { defineAsyncComponent } from "vue";
 
 export default {
   name: 'App',
   components: {
+    Navbar: defineAsyncComponent( () => import(/*webpackChunkName: "Navbar"*/'@/modules/shared/components/Navbar') )
 
   }
 }
