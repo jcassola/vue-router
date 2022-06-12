d5e48f508b331c08fc2c5825fb5d18c87f25b448
Elodin <jcassola96@gmail.com>
2022-06-11 23:37:26 -0400

    custom router links

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,35 @@
 <template>
     <div>
-        <router-link :to="{name: 'home'}">Pokemon List</router-link>
+        <!-- <router-link :to="{name: 'home'}">Pokemon List</router-link>
         <router-link :to="{name: 'pokemon-id', params: {id:1}}">Pokemon por id</router-link>
-        <router-link :to="{name: 'about'}">About</router-link>
+        <router-link :to="{name: 'about'}">About</router-link> -->
+        <custom-link 
+            v-for="link in links"
+            :key="link.to"
+            :link="link"
+        />
     </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    },
+    data(){
+        return {
+            links: [
+                {to: '/home', name: 'Pokemons'},
+                {to: '/pokemonid/50', name: 'Por ID'},
+                {to: '/about', name: 'About'},
+                {to: 'https://google.com', name: 'Google'},
+            ]
+        }
+    }
+}
+</script>
 
 <style scoped>
 
@@ -18,8 +43,8 @@
         margin: 0 10px;
     }
 
-    a.router-link-exact-active{
+    /* a.router-link-exact-active{
         color: #42b983;
-    }
+    } */
 
 </style>
