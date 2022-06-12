f962c580bf12429b4d6432e831e31727306cac6d
Elodin <jcassola96@gmail.com>
2022-06-11 23:06:53 -0400

    make redirect

--- src/router/router.js
+++ src/router/router.js
@@ -4,19 +4,31 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
     {
         path: '/', 
+        redirect: '/home'
+    },
+    {
+        path: '/home', 
+        name: 'home',
         component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') 
     },
     {
-        path: '/about', 
+        path: '/about',
+        name: 'about',
         component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') 
 
     },
     {
-        path: '/id', 
-        component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage') 
+        path: '/pokemonid/:id',
+        name: 'pokemon-id',
+        component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
+        props: (route) => {
+            const id = Number(route.params.id)
+            return isNaN( id ) ? {id: 1} : {id}
+        }
     },
     {
-        path: '/:pathMatch(.*)*', 
+        path: '/:pathMatch(.*)*',
+        name: 'no-found',
         component: () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound') 
     },
 ]
