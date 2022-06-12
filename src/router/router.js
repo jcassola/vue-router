342df4098ed8b86ed5055c545909e47a2e671598
Elodin <jcassola96@gmail.com>
2022-06-12 11:30:57 -0400

    dbz routes

--- src/router/router.js
+++ src/router/router.js
@@ -38,6 +38,30 @@ const routes = [
 
         ]
     },
+    //DBZ routes
+    {
+    path: '/dbz',
+    name: 'dbz',
+    component: () => import(/*webpackChunkName: "DbzLayout"*/'../modules/dbz/layouts/DragonBallLayout'), 
+    children: [
+            {
+                path: 'characters',
+                name: 'dbz-characters',
+                component: () => import(/*webpackChunkName: "DbzCharacters"*/'../modules/dbz/pages/Characters'),
+            },
+            {
+                path: 'about',
+                name: 'dbz-about',
+                component: () => import(/*webpackChunkName: "DbzAbout"*/'../modules/dbz/pages/About'),
+            },
+            {
+                path: '',
+                redirect: {name: 'dbz-characters'}
+            },
+
+        ]
+
+    },
     {
         path: '/:pathMatch(.*)*',
         name: 'no-found',
